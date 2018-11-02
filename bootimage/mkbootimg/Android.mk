9c5354ba694b53cc9f31d79239f33200c74cf9a3
Nemirtingas <nanaki89@hotmail.fr>
2018-11-02 17:03:09 +0100

    Reworking Trees, now in hardware/mrvl/bootimage

--- bootimage/mkbootimg/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include
-LOCAL_SRC_FILES := mkbootimg.c
-LOCAL_STATIC_LIBRARIES := libmincrypt
-LOCAL_MODULE := mkbootimg-pxa
-include $(BUILD_HOST_EXECUTABLE)
